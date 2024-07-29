5502eebc5c93b613c7766b889bfd67ac8f1afdbb
Badri <vtbn012004@gmail.com>
2024-07-29 08:27:47 +0530

    Created UI for homepage, login and signup

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom";
+
+const Logo = ()=>{
+    return(
+        <div>
+            <Link to="/" className="logo">DevJourney</Link>
+        </div>
+    )
+}
+export default Logo;
